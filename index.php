8c72320c5502364aca2078ed539e3ecec2253d80
jonathan-matheus <johhn.dev@gmail.com>
2024-01-26 13:49:39 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body <?php body_class(); ?>>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas vitae
+        commodi perspiciatis harum totam, laudantium nobis, incidunt fugiat
+        dolor delectus, nihil voluptate ullam sequi! Ratione fuga quo animi
+        pariatur veniam.
+    </p>
+    <div id="page" class="site">
+        <!-- CABEÇALHO -->
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="searchbox">
+                    Search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    Menu
+                </nav>
+            </section>
+        </header>
+        <!-- CORPO -->
+        <div id="content" class="site-content">
+            <div id="primary" class="content-area">
+                <main id="main" class="site-main">
+                    <section class="hero">
+                        Hero
+                    </section>
+                    <section class="sevices">
+                        Services
+                    </section>
+                    <section class="home-blog">
+                        Blog
+                    </section>
+                </main>
+            </div>
+        </div>
+        <!-- RODAPÉ -->
+        <footer class="site-footer">
+            Footer
+        </footer>
+    </div>
+</body>
+
+</html>
