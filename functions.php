2199606d5327263d6aa2d9471a2d8458b18a2885
jonathan-matheus <johhn.dev@gmail.com>
2024-02-13 23:14:30 -0300

    Sidebars-1

--- functions.php
+++ functions.php
@@ -80,3 +80,19 @@ function wpdevs_config()
     );
 }
 add_action('after_setup_theme', 'wpdevs_config', 0);
+
+function wpdevs_sidebars()
+{
+    register_sidebar(
+        [
+            'name' => 'Blog Sidebar',
+            'id' => 'sidebar-blog',
+            'description' => 'This is the blog Sidebar. You can add your widgets here.',
+            'before_widget' => '<div class="widget-wrapper">',
+            'after_widget' => '</div>',
+            'before_title' => '<h4 class="widget-title">',
+            'after_title' => '</h4>'
+        ]
+    );
+}
+add_action('widgets_init', 'wpdevs_sidebars');
