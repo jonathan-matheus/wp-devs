ab5d71447d164dfeda120d4d5897cf657251585a
jonathan-matheus <johhn.dev@gmail.com>
2024-03-10 23:38:26 -0300

    Adicionando Comentários - part 1

--- single.php
+++ single.php
@@ -29,6 +29,9 @@ get_header();
                     </div>
                 </article>
             <?php
+                if (comments_open() || get_comments_number()) {
+                    comments_template();
+                }
             }
             ?>
         </div>
