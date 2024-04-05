2ffdc79fa49a240459b0f8ff512ecec490060503
jonathan-matheus <johhn.dev@gmail.com>
2024-04-05 11:33:32 -0300

    Criando página de arquivos, categoria, autor e tags

---
+++ archive.php
@@ -0,0 +1,61 @@
+<!-- Carrega o templete de cabeçalho -->
+<?php get_header(); ?>
+<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" />
+<!-- Corpo -->
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <?php
+            the_archive_title('<h1 class="archive-title">', '</h1>');
+            the_archive_description('<div class="archive-description">', '</div>');
+            ?>
+            <div class="container">
+                <div class="archive-items">
+                    <?php
+                    if (have_posts()) {
+                        while (have_posts()) {
+                            the_post();
+                    ?>
+                            <article>
+                                <h2>
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_title(); ?>
+                                    </a>
+                                </h2>
+                                <div class="meta-info">
+                                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+                                    <p>Categories: <?php the_category(' '); ?></p>
+                                    <p><?php the_tags('Tags: '); ?></p>
+                                </div>
+                                <?php the_excerpt(); ?>
+                            </article>
+                        <?php
+                        }
+                        ?>
+                        <div class="wpdevs-pagination">
+                            <div class="pages new">
+                                <?php
+                                previous_posts_link("<< Newer posts");
+                                ?>
+                            </div>
+                            <div class="pages old">
+                                <?php
+                                next_posts_link("Older posts >>");
+                                ?>
+                            </div>
+                        </div>
+                    <?php
+                    } else {
+                    ?>
+                        <p>Nothing yet to be displayed!</p>
+                    <?php
+                    }
+                    ?>
+                </div>
+                <?php get_sidebar(); ?>
+            </div>
+        </main>
+    </div>
+</div>
+<!-- Carrega o template de rodapé -->
+<?php get_footer(); ?>
